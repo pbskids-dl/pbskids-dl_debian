e18fe3ca919dfb1a169dd22bb6526cb61150f86f
NexusSfan <nexussfan@duck.com>
2024-06-23 10:36:10 -0700

    yooooo

--- setupcli.sh
+++ setupcli.sh
@@ -22,3 +22,5 @@ cp $GITHUB_WORKSPACE/.debian/control ./control
 cd $GITHUB_WORKSPACE
 dpkg --build ./pbskids-dl_deb
 sha256sum pbskids-dl_deb.deb > pbskids-dl.sha256sum
+echo "Build finished!"
+echo "Check for errors after installing package."
