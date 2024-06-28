b2e3f8500ce380fc98d4918ca4b7436403307058
NexusSfan <nexussfan@duck.com>
2024-06-28 00:39:06 +0000

    Update setupgui.sh

--- setupgui.sh
+++ setupgui.sh
@@ -20,4 +20,4 @@ cd ./DEBIAN
 cp $GITHUB_WORKSPACE/.debian/control2 ./control
 cd $GITHUB_WORKSPACE
 dpkg --build ./pbskids-dl_gui_deb
-sha256sum pbskids-dl_gui_deb.deb > pbskids-dl_gui.sha256sum
+sha256sum pbskids-dl_gui_deb.deb | gzip > pbskids-dl_gui.sha256sum.gz
