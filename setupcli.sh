e492a4c6584526579d85940ac48866a9dcb523d7
NexusSfan <nexussfan@duck.com>
2024-07-02 18:37:00 -0700

    Update setupcli.sh

--- setupcli.sh
+++ setupcli.sh
@@ -22,7 +22,6 @@ cd ./DEBIAN
 cp $GITHUB_WORKSPACE/.debian/control ./control
 cd $GITHUB_WORKSPACE
 dpkg --build ./pbskids-dl_cli_deb
-sha256sum pbskids-dl_cli_deb.deb | gzip > pbskids-dl.sha256sum.gz
 sha256sum pbskids-dl_cli_deb.deb > pbskids-dl.sha256sum
 echo "Build finished!"
 echo "Check for errors after installing package."
