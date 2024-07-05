9c2ac893692c80afce36b22b0710d78756d66d86
NexusSfan <nexussfan@duck.com>
2024-07-05 01:45:16 +0000

    Update setupall.sh

--- setupall.sh
+++ setupall.sh
@@ -11,13 +11,6 @@ echo "Building at $(pwd)"
 chmod +x *
 mkdir pbskids-dl_debian_all
 cd ./pbskids-dl_debian_all
-mkdir -p usr/bin
-cd ./usr/bin
-cp $GITHUB_WORKSPACE/pbskids-dl.py .
-cp $GITHUB_WORKSPACE/pbskids-dl.sh .
-ln -s ./pbskids-dl.py ./pbskids-dl
-chmod +x *
-cd ../../
 mkdir DEBIAN
 cd ./DEBIAN
 cp $GITHUB_WORKSPACE/.debian/control3 ./control3
