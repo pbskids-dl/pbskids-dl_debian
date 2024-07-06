4e2754be74d93af8cbc89765ff5583017a5b921b
NexusSfan <nexussfan@duck.com>
2024-07-06 02:41:23 +0000

    Update setupall.sh

--- setupall.sh
+++ setupall.sh
@@ -9,14 +9,23 @@ fi
 cd $GITHUB_WORKSPACE
 echo "Building at $(pwd)"
 chmod +x *
-mkdir pbskids-dl_debian_all
-cd ./pbskids-dl_debian_all
+mkdir pbskids-dl_debian_cli
+cd ./pbskids-dl_debian_cli
+mkdir -p usr/bin
+cd ./usr/bin
+cp $GITHUB_WORKSPACE/pbskids-dl.py .
+cp $GITHUB_WORKSPACE/pbskids-dl.sh .
+ln -s ./pbskids-dl.py ./pbskids-dl
+cp $GITHUB_WORKSPACE/pbskids-dl_gui.py .
+ln -s ./pbskids-dl_gui.py ./pbskids-dl_gui
+chmod +x *
+cd ../../
 mkdir DEBIAN
 cd ./DEBIAN
-cp $GITHUB_WORKSPACE/.debian/control3 ./control3
+cp $GITHUB_WORKSPACE/.debian/control ./control3
 cp $GITHUB_WORKSPACE/.debian/changelog ./changelog
 cd $GITHUB_WORKSPACE
-dpkg --build ./pbskids-dl_debian_all
-sha256sum -b pbskids-dl_debian_all.deb > pbskids-dl.sha256sum
+dpkg-deb --root-owner-group --build ./pbskids-dl_debian
+sha256sum -b pbskids-dl_debian.deb > pbskids-dl_debian.sha256sum
 echo "Build finished!"
 echo "Check for errors after installing package."
