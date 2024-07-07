c620c2180b0ba7e70cb1757c6fb53471334d1aec
NexusSfan <nexussfan@duck.com>
2024-07-07 20:56:44 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -13,12 +13,15 @@ mkdir pbskids-dl_debian
 cd ./pbskids-dl_debian
 mkdir -p usr/lib/pbskids-dl
 cd ./usr/lib/pbskids-dl
+ls -al
+pwd
 cp $GITHUB_WORKSPACE/pbskids-dl.py .
 cp $GITHUB_WORKSPACE/pbskids-dl.sh .
 cp $GITHUB_WORKSPACE/pbskids-dl_gui.py .
 cp $GITHUB_WORKSPACE/.debian/version .
 cd ../../
 mkdir bin
+ls -al pwd
 ln -s ./pbskids-dl.py ../lib/pbskids-dl/pbskids-dl.py
 ln -s ./pbskids-dl_gui.py ../lib/pbskids-dl/pbskids-dl_gui.py
 ln -s ./pbskids-dl_gui.sh ../lib/pbskids-dl/pbskids-dl.sh
